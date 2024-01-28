74f877a4f3eb08fece998577b63ae07fd25fad9e
lulu110997 <louis97fernandez@gmail.com>
2024-01-28 18:22:12 +1100

    final

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def to_SE3(omega, theta, v):
     np.power
     transl = (G@v.transpose()).reshape(3,1)
     return np.vstack((np.hstack((rot, transl)), bot))
-print((np.eye(2)*2)**2)
+
 def get_screw_axis_revolute(s_hat, q):
     theta_dot = 1  # angular velocity
 
@@ -79,7 +79,7 @@ def get_screw_axis_revolute(s_hat, q):
 
 
 def cla():
-    fig = plt.figure(1)
+    fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     # ax.set_xlim(-4.5, 4.5)
     # ax.set_ylim(-0.5, 4.5)
@@ -108,16 +108,37 @@ T3_rot_b = expm(skew_beta(S3, M.A)*pi/2)
 ############################## PLOTS ##############################
 plt.ion(); plt.show()
 
+# 8) Introduce different joint frames
+# 9) Quick mention of how to obtain s_hat and theta (ie the unit axis s_hat and point q are defined in fixed frame)
+# S1 is just s_hat = (0,0,1) as joint axis coincides with fixed frame and q=(0,0,0)
+# S2 s_hat = (0, -1, 0) as the joint's z_axis points to the negative y_fixed frame axis
+# S2 q = [1,0,0]. We could also have chosen (1, 0/2) since we just need any point along z_j2
+# Remember that h=0 since we have zero pitch for revolute joints.
+# ############ another sidenote: Joint 2 has a rot of SE3().Rx(pi/2) @ SE3().Rz(-pi/2)
+# Put in eqn from earlier to obtain screw axis for joint 2. We can then perform the same process to obtain S3
+# Once we have the screw axes and a displacement theta, we can calculate the forward kinematics
 trplot(M.A, frame='ee', color='r', length=0.6);# inter()
 trplot(T3_frame.A, frame='j3', color='g', length=0.5);# inter()
 trplot(T2_frame.A, frame='j2', color='b', length=0.5);# inter()
+
+# 10) Observe how the EE changes as we apply the transforms with pi/2 displacement on each joint. The other joints don't move so we'll have to imagine how it would look
+# 11) Show how the ee pose changes as each operation is applied (pi/2 around the z_axis of each joint). the other joints were not plotted in this case
+# 12) After joint 3 displ, notice how the screw axes of joint 1/2 is still the same relative to the fixed frame
+# 13) for this formulation, notice how M is transform by the joints closest to the EE
 trplot(T1_frame.A, frame='j1,fixedframe', color='black', length=0.5); inter()
 trplot(T3_rot_s@M.A, frame='rot3', color='pink', length=0.7); inter()
 trplot(T2_rot_s@T3_rot_s@M.A, frame='rot3rot2', color='purple', length=0.7); inter()
 trplot(T1_rot_s@T2_rot_s@T3_rot_s@M.A, frame='rot3rot2rot1', color='cyan', length=0.7); inter()
+ax = plt.gca()
+ax.view_init(elev=100, azim=0, roll=0); inter()
 
 cla()
-
+# 14) 2 ways: 1 screw axes defined in body frame (ee in this case) or just convert the screw axes represented in the
+# fixed frame to be represented in the body frame. More details in book
+# 15) Show how the ee pose changes as each operation is applied (pi/2 around the z_axis of each joint)
+# 16) Didn't plot but joint 2 also moves
+# 17) After joint 1 displ, notice how the screw axes of joint 2/3 is still the same relative to the body (ee) frame
+# 18) for this formulation, notice how M is transform by the joints closest to the EE
 trplot(M.A, frame='ee', color='r', length=0.6);# inter()
 trplot(T3_frame.A, frame='j3', color='g', length=0.5);# inter()
 trplot(T2_frame.A, frame='j2', color='b', length=0.5);# inter()
@@ -125,8 +146,14 @@ trplot(T1_frame.A, frame='j1,fixedframe', color='black', length=0.5); inter()
 trplot(M.A@T1_rot_b, frame='rot3', color='pink', length=0.7); inter()
 trplot(M.A@T1_rot_b@T2_rot_b, frame='rot3rot2', color='purple', length=0.7); inter()
 trplot(M.A@T1_rot_b@T2_rot_b@T3_rot_b, frame='rot3rot2rot1', color='cyan', length=0.7); inter()
+ax = plt.gca()
+ax.view_init(elev=100, azim=0, roll=0)
 
+print(np.round(T1_rot_s@T2_rot_s@T3_rot_s@M.A, 3))
+print(np.round(M.A@T1_rot_b@T2_rot_b@T3_rot_b, 3))
 
+ax = plt.gca()
+ax.view_init(elev=100, azim=0, roll=0); inter()
 # Visualise joint reference frames
 # trplot(T1_frame.A, frame='inital j1', color='r', length=0.5);# inter()
 # trplot(T2_frame.A, frame='inital j2', color='g', length=0.5);# inter()
